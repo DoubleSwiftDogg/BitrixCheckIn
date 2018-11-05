f12cc1697daf6a2ebb41c0b5f7a2c11e3c55eba3
MacBook <macbook@macbook-pro-eldorado.local>
2018-11-05 12:14:37 +0300

    Версия с открытием браузера и авторизацией.

--- BitrixCheckIn/WebViewController.swift
+++ BitrixCheckIn/WebViewController.swift
@@ -30,24 +30,18 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         
+     
+        
         let savedLogin = "sysoevpb"
         let savedPassword = "5594262"
         
         let fillForm = String(format: "document.getElementsByName('USER_PASSWORD')[0].value = '\(savedPassword)';document.getElementsByName('USER_LOGIN')[0].value = '\(savedLogin)';")
         webView.evaluateJavaScript(fillForm, completionHandler: nil)
+        
         //submit form
         //dispatch_after(dispatch_time(UInt64(DISPATCH_TIME_NOW), Int64(1 * NSEC_PER_SEC)), dispatch_get_main_queue()){
         webView.evaluateJavaScript("document.forms[\"form_auth\"].submit();")
+        
     }
-    
-    
-    /* func webViewDidFinishLoad(webView: UIWebView) {
-        
-        
-        let savedPassword = "5594262"
-        
-        let fillForm = String(format: "document.getElementById('USER_PASSWORD').value = '\(savedPassword)';")
-        webView.stringByEvaluatingJavaScript(from: fillForm)
-    } */
 
 }
